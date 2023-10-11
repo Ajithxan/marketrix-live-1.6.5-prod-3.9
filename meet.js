38d588fbae90d572b021a1fb60c646bf22395a91
Ajithxan <Ajithshanth.creativehub@gmail.com>
2023-10-11 10:53:51 +0530

    cdn link

--- meet.js
+++ meet.js
@@ -1,5 +1,6 @@
 const meetVersion = "1.6.5-prod-1.9"
-const CDNlink = 'http://localhost/creativehub/marketrix-live-1.3.4/' //`https://cdn.jsdelivr.net/gh/Ajithxan/marketrix-live-${meetVersion}/`
+// const CDNlink = 'http://localhost/creativehub/marketrix-live-1.3.4/' 
+const CDNlink = `https://cdn.jsdelivr.net/gh/Ajithxan/marketrix-live-${meetVersion}/`
 console.log(CDNlink)
 const startingTime = new Date().getTime()
 const fontAwesomeCDNLink = document.createElement("link")
