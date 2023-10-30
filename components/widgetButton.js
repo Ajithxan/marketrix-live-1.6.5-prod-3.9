df9f1069f45507e0dd19d883fd0a8628073a2dc6
= <sandini.creativehub@gmail.com>
2023-10-30 12:49:00 +0530

    card added

--- components/widgetButton.js
+++ components/widgetButton.js
@@ -1,30 +1,30 @@
 class WidgetButton extends HTMLElement {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.text = "Live Agent"
-        this.bg_color = "white"
-        this.logo_url = "{{CDN_LINK}}assets/images/mtx-logo.jpg"
-        this.text_color = "black"
+    this.text = "Live Agent";
+    this.bg_color = "white";
+    this.logo_url = "{{CDN_LINK}}assets/images/mtx-logo.jpg";
+    this.text_color = "black";
+  }
+
+  connectedCallback() {
+    this.fetchData();
+    this.render();
+  }
+
+  fetchData() {
+    const data = ROUTE.componentData("widget_button");
+    if (data) {
+      this.text = data["text"];
+      this.bg_color = data["bg_color"];
+      this.logo_url = data["logo_url"];
+      this.text_color = data["text_color"];
     }
+  }
 
-    connectedCallback() {
-        this.fetchData()
-        this.render()
-    }
-
-    fetchData() {
-        const data = ROUTE.componentData("widget_button")
-        if (data) {
-            this.text = data['text']
-            this.bg_color = data['bg_color']
-            this.logo_url = data['logo_url']
-            this.text_color = data['text_color']
-        }
-    }
-
-    render() {
-        this.innerHTML = `<div class="mtx-mainButton" id="marketrix-button" style="background-color:${this.bg_color}">
+  render() {
+    this.innerHTML = `<div class="mtx-mainButton" id="marketrix-button" style="background-color:${this.bg_color}">
                 <div class="mtx-button" onclick="showModal()">
                     <div class="mtx-avatar">
                         <div>
@@ -34,8 +34,8 @@ class WidgetButton extends HTMLElement {
                     </div>
                     <div class="mtx-button-txt" style="color:${this.text_color}">${this.text}</div>
                 </div>
-            </div>`
-    }
+            </div>`;
+  }
 }
 
-customElements.define("widget-button", WidgetButton)
+customElements.define("widget-button", WidgetButton);
--- components/widgetCard.js
+++ components/widgetCard.js
@@ -1,27 +1,33 @@
 class WidgetCard extends HTMLElement {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.text = "Widget card text"
-    }
+    this.text = "Widget card text";
+    this.themeColor = "purple";
+  }
 
-    connectedCallback() {
-        this.fetchData()
-        this.render()
-    }
+  connectedCallback() {
+    this.fetchData();
+    this.render();
+  }
 
-    fetchData() {
-        const data = ROUTE.componentData("widget_card")
-        if (data) {
-            console.log(data)
-        }
+  fetchData() {
+    const data = ROUTE.componentData("widget_card");
+    if (data) {
+      console.log(data);
     }
+  }
 
-    render() {
-        this.innerHTML = `<div class="mtx-mainButton">
-                ${this.text}
-            </div>`
-    }
+  render() {
+    this.innerHTML = `
+    <div class="mtx-snpt-video-circle-comp" onclick="showModal()"> 
+    <p>Press Right arrow key  to start the journey 👉🏼 </p>
+ <div class="mtx-video-circle"> 
+          
+ <video muted autoplay loop>
+   <source src="{{CDN_LINK}}assets/images/hellovideo.mp4" type="video/mp4">
+ </video>`;
+  }
 }
 
-customElements.define("widget-card", WidgetCard)
+customElements.define("widget-card", WidgetCard);
